e25aabe132a18c340fb864f8135b6f793683c610
aiyion.prime <git@aiyionpri.me>
2025-03-27 23:00:45 +0100

    test: Add integration test for an arbitrary v2 nodes.json
    shipped as test resources.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,15 @@
 use nodes_parse::NodesJSON;
+use std::path::PathBuf;
+
+#[test]
+fn test_arbitrary_static_v2() {
+    use std::env;
+    let mut path_buf = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    path_buf.push("resources/tests/nodes.json");
+    let data = std::fs::read_to_string(path_buf).unwrap();
+    let nodes_json = serde_json::from_str::<NodesJSON>(&data).unwrap();
+    assert! {nodes_json.version==2}
+}
 
 #[test]
 fn test_parse_current_v2() {
