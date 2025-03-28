c7de813c9674b061a7d80f04cf4876b9b3ac6938
aiyion.prime <git@aiyionpri.me>
2025-03-28 20:14:01 +0100

    refactor: Reformat sourcecode

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -15,9 +15,11 @@ fn test_arbitrary_static_v2() {
 fn test_parse_current_v2() {
     let url = "https://harvester.ffh.zone/api/nodes.json";
     let response: String = ureq::get(url)
-        .call().unwrap()
+        .call()
+        .unwrap()
         .body_mut()
-        .read_to_string().unwrap();
+        .read_to_string()
+        .unwrap();
     let nodes_json: NodesJSON = serde_json::from_str(&response).unwrap();
-    assert!{nodes_json.version==2}
+    assert! {nodes_json.version==2}
 }
